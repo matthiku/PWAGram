12425b715de4eb42a5ce081cdc5d25927381274f
Matthias Kuhs <matthiku@yahoo.com>
2018-05-28 09:03:35 +0100

    caching strategies

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -26,9 +26,28 @@ function closeCreatePostModal() {
   createPostArea.style.display = 'none';
 }
 
+// allows to save items on demand
+function onSaveButtonClicked(event) {
+  console.log('save button clicked!');
+  if ('caches' in window){
+    caches.open('user-requested')
+      .then(function (cache) {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
+  }
+}
+
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function clearCards() {
+  // remove all children from the current list
+  while (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
 
 function createCard() {
   var cardWrapper = document.createElement('div');
@@ -48,15 +67,46 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+
+  var cardSaveButton = document.createElement('button');
+  cardSaveButton.textContent = 'save';
+  cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
+
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://httpbin.org/get')
+/**
+ *  implement the "Cache first, then Web" strategy
+ */
+var url = 'https://httpbin.org/get';
+var networkDataReceived = false;
+
+fetch(url)
   .then(function(res) {
     return res.json();
   })
   .then(function(data) {
+    console.log('from Web:', data);
+    networkDataReceived = true;
+    clearCards();
     createCard();
   });
+
+if ('caches' in window) {
+  caches.match(url)
+  .then(function (response) {
+    if (response) {
+      return response.json();
+    }
+  })
+  .then(function (data) {
+    console.log('from Cache', data);
+    // only use the cached data if we didn't receive the data from the network
+    if (!networkDataReceived)
+      clearCards();
+      createCard();
+  });
+}
--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,28 @@
-var CACHE_STATIC_NAME = 'static-v4';
-var CACHE_DYNAMIC_NAME = 'dynamic';
+var CACHE_STATIC_NAME = 'static-v13';
+var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+var STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
-  // make sure the cache is filled before we finish the installation event
+  // wait until the caching is done before we finish the installation event
   event.waitUntil(
-    caches.open(CACHE_STATIC_NAME) // user versioning to prevent the cache becoming outdated
+    caches.open(CACHE_STATIC_NAME) // use versioning to prevent the cache becoming outdated
       .then(function (cache) {
         console.log('[Service Worker] Pre-caching App shell');
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
+        cache.addAll(STATIC_FILES);
       })
   );
 });
@@ -28,6 +30,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
   // here we can cleanup old and outdated caches
+  // yet let the 'activate' event wait until this step is done
   event.waitUntil(
     caches.keys()
       .then(function (keyList) {
@@ -40,34 +43,76 @@ self.addEventListener('activate', function(event) {
           }
         }));
       })
-  )
+  );
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // check if we have the response to this request already in our cache
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          console.log('[Service Worker] fetching this from Cache -', event.request.url);
-          return response;
-        } else {
-          // request/response wasn't found, so we make the request and return the response,
-          // but also store the repsponse in our cache
+// helper function to check if URL is found in the provided array
+const isInCache = (requestURL, cacheArr) => cacheArr.some(url => url === requestURL.replace(self.origin, ''));
+
+self.addEventListener('fetch', function (event) {
+
+  // requests like API calls need to have their own strategy
+  var url = 'https://httpbin.org/get';
+
+  /**
+   * A) Use Cache AND make a Network fetch, then cache everything!
+   */
+  if (event.request.url.indexOf(url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME)
+        .then(function (cache) {
           return fetch(event.request)
-            .then(function (resp) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function (cache) {
-                  // we need to clone the response as response can only used once
-                  cache.put(event.request.url, resp.clone());
-                  return resp;
-                });
-            })
-            .catch(function (err) {
-              console.log(err);
+            .then(function (res) {
+              cache.put(event.request, res.clone());
+              return res;
             });
-        }
-      })
-  );
+        })
+    );
+
+  // check if the requested url is part of the static assets
+  } else if (isInCache(event.request.url, STATIC_FILES)) {
+    // then use the "cache-only" strategy
+    event.respondWith(
+      caches.match(event.request)
+    );
+  } else {
+    /**
+     * B) Try cache first and only do a Network fetch if not found - then cache it
+     */
+    event.respondWith(
+      // check if we have the response to this request already in our cache
+      caches.match(event.request)
+        .then(function (response) {
+          if (response) {
+            // the request was found in the cache!
+            console.log('[Service Worker] fetching this from Cache -', event.request.url);
+            return response;
+          } else {
+            // request/response wasn't found, so we make the network request and return the response,
+            // but also store the repsponse in our cache
+            return fetch(event.request)
+              .then(function (resp) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                  .then(function (cache) {
+                    // we need to clone the response as response can only used once
+                    cache.put(event.request.url, resp.clone());
+                    console.log('[Service Worker] adding to dynamic cache -', event.request.url);
+                    return resp; // return the response to the web page
+                  });
+              })
+              .catch(function (err) {
+                // use a fallback page if we haven't cached the requested page yet
+                return caches.open(CACHE_STATIC_NAME)
+                  .then(function (cache) {
+                    // only return the default page if it makes sense (e.g. not for CSS files)
+                    if (event.request.url.endsWith('/help'))
+                      return cache.match('/offline.html');
+                  });
+              });
+          }
+        })
+    );
+  }
+
 });
