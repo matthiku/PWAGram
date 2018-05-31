6bcc37ddc31dd3e6561a97efc82d1dfe72730f75
Matthias Kuhs <matthiku@yahoo.com>
2018-05-31 13:41:25 +0100

    correcting cloud function

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,21 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({origin: true});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+// see https://console.firebase.google.com/u/0/project/pwagramma/settings/serviceaccounts/adminsdk
+var serviceAccount = require('./pwagramma-firebase-key.json');
+
+admin.initializeApp({
+  // where is the DB
+  databaseURL: 'https://pwagramma.firebaseio.com/',
+  // key file
+  credential: admin.credential.cert(serviceAccount)
+});
 
 exports.storePostData = functions.https.onRequest((request, response) => {
-  cors(function (request, response) {
+  cors(request, response, function() {
     admin.database().ref('posts').push({
       id: request.body.id,
       title: request.body.title,
