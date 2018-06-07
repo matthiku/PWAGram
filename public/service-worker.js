2776ca0dcab4a753a0d45b569a373816dc56e13f
Matthias Kuhs <matthiku@yahoo.com>
2018-06-07 13:50:26 +0100

    final course project

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,13 @@
 /* jshint esversion: 6 */
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
-importScripts('/src/js/idb.js');
-importScripts('/src/js/utility.js');
 
+importScripts('/src/js/idb.js'); // indexedDB package
+importScripts('/src/js/utility.js'); // our own utilities
+
+/**
+ * CACHING
+ */
 // static files
 workbox.precaching.precacheAndRoute([
   {
@@ -68,7 +72,7 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "sw-base.js",
-    "revision": "30679c3f4792488421073264e457fa42"
+    "revision": "a859dfe7ca4409dcba7776b30389e063"
   },
   {
     "url": "sw.js",
@@ -96,7 +100,6 @@ workbox.precaching.precacheAndRoute([
   }
 ]);
 
-
 // images from posts
 workbox.routing.registerRoute(
   /.*(?:firebasestorage\.googleapis)\.com.*$/,
@@ -121,9 +124,11 @@ workbox.routing.registerRoute(
   })
 );
 
-// own caching strategy for database get requests
+// own caching strategy for backend database GET requests
 workbox.routing.registerRoute(
-  /.*(?:pwagramma\.firebaseio\.com\/posts|pwagramma\.firebaseio\.com\/subscriptions)\.json.*$ / ,
+
+  /.*(?:pwagramma\.firebaseio\.com\/posts|pwagramma\.firebaseio\.com\/subscriptions)\.json.*$/,
+
   (args) => {
     return fetch(args.event.request)
       .then(function (res) {
@@ -134,7 +139,7 @@ workbox.routing.registerRoute(
           })
           .then(function (data) {
             // loop through each item in the data object from firebase
-            console.log('[Service Worker] cloned response from fetching', event.request.url, data);
+            console.log('[Service Worker] cloned response from fetching', args.event.request.url, data);
             for (var key in data) {
               writeData('posts', data[key]);
             }
@@ -145,5 +150,154 @@ workbox.routing.registerRoute(
 );
 
 
+
+// providing a fallback page for HTML(!) content that is not cached
+workbox.routing.registerRoute(
+
+  // function must return true in order for this route to apply
+  (routeData) => {
+    // check if the request is accepting HTML pages as a response
+    return (routeData.event.request.headers.get('accept').includes('text/html'));
+  },
+
+  (args) => {
+    // check if we have the response to this request already in our cache
+    return caches.match(args.event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          // the request was found in the cache!
+          console.log('[Service Worker] fetching this from Cache -', args.event.request.url);
+          return cachedResponse;
+        } else {
+          // request/response wasn't found, so we make the network request and return the response,
+          // but also store the repsponse in our cache
+          return fetch(args.event.request)
+            .then((resp) => {
+              return caches.open('dynamic')
+                .then((cache) => {
+                  // trimCache(CACHE_DYNAMIC_NAME, 9);
+                  // we need to clone the response as responses can only be used once!
+                  cache.put(args.event.request.url, resp.clone());
+                  console.log('[Service Worker] adding to dynamic cache -', args.event.request.url);
+                  return resp; // return the response to the web page
+                });
+            })
+            .catch(function (err) {
+              // use a fallback page if we haven't cached the requested page yet
+              return caches.match('/offline.html')
+                .then( (cachedPage) => {
+                  return cachedPage;
+                });
+            });
+        }
+      });
+  }
+);
+
+
+/**
+ * Listen to SYNC events
+ * 
+ * and act depending on the tag name of the sync event 
+ */
+self.addEventListener('sync', function (event) {
+  console.log('[Service Worker] Background Syncing,', event);
+  if (event.tag === 'sync-new-post') {
+    console.log('[Service Worker]  Syncing new post');
+    event.waitUntil(
+      // get all posts waiting to be synced
+      readAllData('syncPosts')
+      .then(function (data) {
+        // loop through each single post
+        for (var dt of data) {
+          sendData(backendUrl, dt)
+            .then(function (res) {
+              console.log('[Service Worker]  New post was sent', res);
+              // clean up the post that has successfuly been sent to the backend
+              if (res.ok) {
+                res.json()
+                  .then(function (resData) {
+                    deleteItemFromData('syncPosts', resData.id);
+                  });
+              }
+            });
+        }
+      })
+    );
+  }
+});
+
+
+/**
+ * Listen to CLICK events on Notifications
+ */
+self.addEventListener('notificationclick', function (event) {
+  console.log(event);
+  var notification = event.notification;
+  if (event.action === 'confirm') {
+    // do nothing else for now...
+    notification.close();
+  } else {
+    console.log(event.action);
+    // open a new window (or tab) in the browser
+    event.waitUntil(
+      // refers to all windows or browser tasks related to this service worker
+      clients.matchAll()
+      .then((clientsArr) => {
+        // find windows which are open or visible
+        var client = clientsArr.find((c) => {
+          return c.visibiityState === 'visible';
+        });
+        if (client !== undefined) {
+          client.navigate(notification.data.url);
+          client.focus();
+        } else {
+          clients.openWindow(notification.data.url);
+        }
+        notification.close();
+      })
+    );
+  }
+});
+
+/**
+ * Listen to CLOSE events from Notifications
+ */
+self.addEventListener('notificationclose', function (event) {
+  console.log(event);
+});
+
+
+/**
+ * Listen to PUSH NOTIFICATIONS 
+ */
+self.addEventListener('push', (event) => {
+
+  // receive the notification
+  var data = { // initial dummy values
+    title: 'New',
+    content: 'Something new happended',
+    openUrl: '/',
+    preview: '/src/images/icons/app-icon-96x96.png'
+  };
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+  console.log('Push notification received', data);
+
+  // show the notification
+  var options = {
+    body: data.content,
+    icon: '/src/images/icons/app-icon-96x96.png',
+    badge: '/src/images/icons/app-icon-96x96.png',
+    image: data.preview,
+    data: {
+      url: data.openUrl
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
 
 
